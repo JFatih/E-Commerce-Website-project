6edd3f41628bb8ebaa4050fdd21fa05ff9caa4af
fatih-cakmak <cakmak.35f@gmail.com>
2024-04-25 11:41:37 +0300

    update

--- src/layout/Slider2.jsx
+++ src/layout/Slider2.jsx
@@ -16,18 +16,18 @@ export default function Slider2() {
         navigation={true}
         className="h-[1230px] lg:h-[709px] "
       >
-        <SwiperSlide className=" bg-secondaryColor static overflow-hidden">
+        <SwiperSlide className=" bg-secondaryColor static overflow-hidden flex">
           <img
             src="/simpleman.png"
             className="bottom-0 absolute lg:right-72 object-center object-cover scale-105 "
           />
-          <div className="absolute inset-0 text-white flex flex-col justify-start items-center py-64  text-center gap-3 lg:p-40 lg:text-start">
+          <div className="  text-white flex flex-col justify-start items-center py-64  text-center gap-3  lg:w-6/12 lg:text-start border border-black">
             <p className="sh5">SUMMER 2024</p>
             <h1 className="sh2 md:sh1 ">Vita Classic Product</h1>
             <p className="sh4 w-72   text-lightTextGray">
               We know how large objects will act, but things on a small scale
             </p>
-            <div className="flex lg:flex-row lg:justify-between flex-col gap-3">
+            <div className="flex flex-col gap-8">
               <p className="sh3 text-lightTextGray">$16,48</p>
               <Link to="/shop">
                 <button className="sh3 bg-successColor text-white  py-3 px-8 rounded">
