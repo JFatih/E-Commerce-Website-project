9f7c2172b3c47aa30f24d6c8b1e4a9ac0772c504
fatih-cakmak <cakmak.35f@gmail.com>
2024-05-11 22:53:46 +0300

    update v3.1

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,10 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux"; // legacy_createStore'ı kullanacaksanız, createStore'ı doğru şekilde içe aktarın
-import thunk from "redux-thunk"; // redux-thunk'u doğru şekilde içe aktarın
-import logger from "redux-logger";
-import { combineReducers } from "redux"; // combineReducers'ı doğru şekilde içe aktarın
+import { legacy_createStore as createStore } from "redux";
+import { combineReducers, applyMiddleware } from "redux";
 import ClientReducer from "./reducers/ClientReducer.jsx";
 import ProductReducer from "./reducers/ProductReducer.jsx";
 import ShoppingCartReducer from "./reducers/ShoppingCartReducer.jsx";
+import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
 const reducers = combineReducers({
   Client: ClientReducer,
