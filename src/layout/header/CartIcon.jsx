b3bf878e11a6d19ef2a00bd634789cb2197d7f02
fatih-cakmak <cakmak.35f@gmail.com>
2024-06-21 00:55:14 +0300

    Update cartColor

--- src/layout/header/CartIcon.jsx
+++ src/layout/header/CartIcon.jsx
@@ -7,16 +7,19 @@ export default function CartIcon() {
   return (
     <div className="relative group">
       <i className="fa-solid fa-cart-shopping"></i>
-      <div className="absolute hidden group-hover:block rounded-md border-t-0 z-10 p-2 sparagraph right-0 bg-white">
-        <div className="flex flex-col gap-3">
+      <div className="absolute hidden group-hover:block rounded-md border border-mutedTextColor z-10 p-2 sparagraph right-0 bg-white">
+        <div className="flex flex-col gap-2">
           <p className="text-start">Sepetim: {cartData.length} ürün</p>
           {cartData.map((data, index) => {
             return (
               <div
                 key={index}
-                className="flex flex-row gap-3 justify-center items-center border border-color3 w-60"
+                className="flex flex-row gap-3 justify-start items-center border border-mutedTextColor w-60"
               >
-                <img src={data.product.images[0].url} className="w-14 " />
+                <img
+                  src={data.product.images[0].url}
+                  className="w-14 h-24 mx-3 object-contain"
+                />
                 <div className="flex flex-col gap-2">
                   <p>{data.product.name}</p>
                   <p>Count: {data.count}</p>
@@ -26,7 +29,7 @@ export default function CartIcon() {
             );
           })}
           <Link to="/cart">
-            <button className="bg-color3 text-white rounded-md py-1 px-3 block">
+            <button className="bg-color3 text-white rounded-md py-1 px-3 block mx-2">
               Go to Cart
             </button>
           </Link>
