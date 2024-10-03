ed211cbdd2f6a04a6b5b3e3969c719ba1145bf1a
fatih-cakmak <cakmak.35f@gmail.com>
2024-10-03 16:52:56 +0300

    Credit Cart part start

--- src/pages/Payment/PaymentSection/PaymentSection.jsx
+++ src/pages/Payment/PaymentSection/PaymentSection.jsx
@@ -1,7 +1,21 @@
-export default function PaymentSection() {
+import { useEffect, useState } from "react";
+import PaymentSelector from "./PaymentSelector";
+
+export default function AddressSection() {
+  const [paymentData, setPaymentData] = useState(null);
+
+  useEffect(() => {}, []);
+
   return (
-    <div>
-      <p>Payment section</p>
+    <div className="border rounded-sm shadow-md flex flex-col justify-start items-center sh5 gap-1 pb-3">
+      <div className="w-full flex flex-col gap-5 lg:flex-row lg:px-3 lg:p-1 items-center lg:items-start">
+        <div className="lg:w-full max-w-[331px]">
+          <div className="py-1 flex flex-col justify-center items-center">
+            <p className="text-center py-1 text-alertColor sh3">Payment Data</p>
+          </div>
+          <PaymentSelector />
+        </div>
+      </div>
     </div>
   );
 }
